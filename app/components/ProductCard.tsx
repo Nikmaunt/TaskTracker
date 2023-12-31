1d4ff8e973e669054da2a74eb90661b836defd7b
Nick <nikmaunt@gmail.com>
2023-12-31 22:36:45 +0400

    feat: product card

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import AddToCart from "@/app/components/AddToCart";
 
+
 const ProductCard = () => {
     return (
-        <div>
+        <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-slate-500'>
             <AddToCart/>
         </div>
     );
