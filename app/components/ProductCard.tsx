63767e7b05290e87d1449933e2692d0254a6f4bb
Nick <nikmaunt@gmail.com>
2024-01-01 16:19:22 +0400

    feat: styling

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,7 +4,7 @@ import AddToCart from "@/app/components/AddToCart";
 
 const ProductCard = () => {
     return (
-        <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-slate-500'>
+        <div>
             <AddToCart/>
         </div>
     );
